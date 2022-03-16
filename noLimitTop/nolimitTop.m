91425a9c907fd54092a660b597e634ca5d2472f5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2022-03-16 14:56:45 -0700

    correct lingo

--- noLimitTop/nolimitTop.m
+++ noLimitTop/nolimitTop.m
@@ -1,34 +1,34 @@
 getUniformMap[edo_, limit_] := Map[Round[edo * Log[2, #]]&, Map[Prime, Range[PrimePi[limit]]]];
 
-m = {1, 1};
+map = {1, 1};
 results = "";
 
 Do[
-  mPrevious = {};
+  mapPrevious = {};
   While[
-    m != mPrevious,
+    map != mapPrevious,
     
-    mPrevious = m;
+    mapPrevious = map;
     
-    mWeighted = MapIndexed[First[#1 / Log[2, Prime[#2]]]&, m];
-    narrowestPrimeIndex = First[First[Position[mWeighted, Min[mWeighted]]]];
-    widestPrimeIndex = First[First[Position[mWeighted, Max[mWeighted]]]];
+    mapWeighted = MapIndexed[First[#1 / Log[2, Prime[#2]]]&, map];
+    narrowestPrimeIndex = First[First[Position[mapWeighted, Min[mapWeighted]]]];
+    widestPrimeIndex = First[First[Position[mapWeighted, Max[mapWeighted]]]];
     narrowestPrime = Prime[narrowestPrimeIndex];
     widestPrime = Prime[widestPrimeIndex];
-    narrowestPrimeMapping = m[[narrowestPrimeIndex]];
-    widestPrimeMapping = m[[widestPrimeIndex]];
+    narrowestPrimeMapping = map[[narrowestPrimeIndex]];
+    widestPrimeMapping = map[[widestPrimeIndex]];
     
     edo = (narrowestPrimeMapping * Log[2, widestPrime] + widestPrimeMapping * Log[2, narrowestPrime]) / (2 * Log[2, narrowestPrime] * Log[2, widestPrime]);
     
     damage = Abs[(narrowestPrimeMapping / edo) / Log[2, narrowestPrime] - 1];
     maximumPrimePossibleToExceedDamage = Min[1000, Power[2, 1 / (2 * damage * edo)] + 2];
     
-    m = getUniformMap[edo, maximumPrimePossibleToExceedDamage];
+    map = getUniformMap[edo, maximumPrimePossibleToExceedDamage];
   ];
   
-  results = results <> ToString[N[edo, 17]] <> " " <> ToString[m] <> "\n";
+  results = results <> ToString[N[edo, 17]] <> " " <> ToString[map] <> "\n";
   
-  m[[narrowestPrimeIndex]]++,
+  map[[narrowestPrimeIndex]]++,
   
   1000
 ];
