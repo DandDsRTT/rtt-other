c2c0ac7547983783f4100d04768c3214c653da5a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2022-03-25 17:02:25 -0700

    making it a bit easier to visualize different tunings in this way, though it still could use plenty of automation and imprevemetn

--- tuning/new/paulAugene.m
+++ tuning/new/paulAugene.m
@@ -1,66 +1,67 @@
-Plot3D[
-  {
-    (*2/1*) Abs[1200.000 - (3 * g1 + 0 * g2)] / Log[2, 2],
-    (*3/1*) Abs[1901.955 - (0 * g1 + 1 * g2)] / Log[2, 3],
-    (*5/1*) Abs[2786.314 - (7 * g1 + 0 * g2)] / Log[2, 5],
-    (*7/1*) Abs[3368.826 - (18 * g1 + -2 * g2)] / Log[2, 7]
-  },
-  {g1, 396, 402},
-  {g2, 1890, 1917},
-  PlotTheme -> "BoldColor",
-  PlotRange -> {0, 40},
-  MaxRecursion -> 6
-]
+m = {{3, 0, 7, 0}, {0, 4, 0, 7}};
+(*m = {{3,0,7,18},{0,1,0,-2}};*)
+m = {{5, 8, 0}, {0, 0, 1}};
 
-In[257] := Plot3D[
-  {
-    (*2/1*) Abs[1200.000 - (3 * g1 + 0 * g2)] / Log[2, 2]
-  },
-  {g1, 396, 402},
-  {g2, 1890, 1917},
-  PlotTheme -> "BoldColor",
-  PlotRange -> {0, 40},
-  MaxRecursion -> 6
-]
+(* TODO: would be better if this automatically asisgned the g mins and maxes below *)
+optimizeGtm[{m, "co"}, "tuning" -> "Tenney"]
+
+g1min = 237;
+g1max = 241;
+g2min = 2750;
+g2max = 2850;
+
 
 Plot3D[
   {
-    0,
-    (*3/1*) Abs[1901.955 - (0 * g1 + 1 * g2)] / Log[2, 3]
+    (*2/1*)Abs[1200.000 - (m[[1]][[1]] * g1 + m[[2]][[1]] * g2)],
+    (*3/1*) Abs[1901.955 - (m[[1]][[2]] * g1 + m[[2]][[2]] * g2)] / Log[2, 3],
+    (*5/1*) Abs[2786.314 - (m[[1]][[3]] * g1 + m[[2]][[3]] * g2)] / Log[2, 5](*,
+    (*7/1*) Abs[3368.826 - (m[[1]][[4]] * g1 + m[[2]][[4]] * g2)]/ Log[2,7]*)
   },
-  {g1, 396, 402},
-  {g2, 1890, 1917},
-  PlotTheme -> "BoldColor",
-  PlotRange -> {0, 40},
-  MaxRecursion -> 6,
-  PlotStyle -> {Opacity[0], Opacity[1]}
+  {g1, g1min, g1max},
+  {g2, g2min, g2max},
+  PlotTheme -> "BoldColor", PlotRange -> {0, 40}, MaxRecursion -> 6
 ]
 
-In[259] := Plot3D[
+Plot3D[
+  {
+    (*2/1*) Abs[1200.000 - (m[[1]][[1]] * g1 + m[[2]][[1]] * g2)]
+  },
+  {g1, g1min, g1max},
+  {g2, g2min, g2max},
+  PlotTheme -> "BoldColor", PlotRange -> {0, 40}, MaxRecursion -> 6
+]
+
+(*Print["hhm", m[[1]][[2]] , "and", m[[2]][[2]]];*)
+Plot3D[
+  {
+    0,
+    (*3/1*) Abs[1901.955 - (m[[1]][[2]] * g1 + m[[2]][[2]] * g2)] / Log[2, 3]
+  },
+  {g1, g1min, g1max},
+  {g2, g2min, g2max},
+  PlotTheme -> "BoldColor", PlotRange -> {0, 40}, PlotStyle -> {Opacity[0], Opacity[1]}, MaxRecursion -> 6
+]
+
+Plot3D[
   {
     0,
     0,
-    (*5/1*) Abs[2786.314 - (7 * g1 + 0 * g2)] / Log[2, 5]
+    (*5/1*) Abs[2786.314 - (m[[1]][[3]] * g1 + m[[2]][[3]] * g2)] / Log[2, 5]
   },
-  {g1, 396, 402},
-  {g2, 1890, 1917},
-  PlotTheme -> "BoldColor",
-  PlotRange -> {0, 40},
-  MaxRecursion -> 6,
-  PlotStyle -> {Opacity[0], Opacity[0], Opacity[1]}
+  {g1, g1min, g1max},
+  {g2, g2min, g2max},
+  PlotTheme -> "BoldColor", PlotRange -> {0, 40}, PlotStyle -> {Opacity[0], Opacity[0], Opacity[1]}, MaxRecursion -> 6
 ]
 
-In[260] := Plot3D[
-  {
-    0,
-    0,
-    0,
-    (*7/1*) Abs[3368.826 - (18 * g1 + -2 * g2)] / Log[2, 7]
+(*Plot3D[
+ {
+ 0,
+ 0,
+ 0,
+    (*7/1*) Abs[3368.826 - (m[[1]][[4]] * g1 + m[[2]][[4]] * g2)]/ Log[2,7]
   },
-  {g1, 396, 402},
-  {g2, 1890, 1917},
-  PlotTheme -> "BoldColor",
-  PlotRange -> {0, 40},
-  MaxRecursion -> 6,
-  PlotStyle -> {Opacity[0], Opacity[0], Opacity[0], Opacity[1]}
-]
+    {g1, g1min, g1max},
+  {g2, g2min, g2max},
+  PlotTheme -> "BoldColor", PlotRange -> {0,40}, PlotStyle -> {Opacity[0], Opacity[0],Opacity[0],Opacity[1]}, MaxRecursion -> 6
+]*)
