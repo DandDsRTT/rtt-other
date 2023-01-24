2fc3931689beb8cefbb62c1ddb918f3f2e051abb
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2023-01-24 10:43:20 -0800

    latest stuff

--- tuning/newer/decipheringAdvancedTiebreakingMaxPolytope.m
+++ tuning/newer/decipheringAdvancedTiebreakingMaxPolytope.m
@@ -67,18 +67,25 @@ graphTuningDamage[unparsedT_, tuningSchemeSpec_, optimumGeneratorTuningMap_] := 
   (* other settings *)
   AppendTo[plotArgs, ImageSize -> 1000];
   (*AppendTo[plotArgs, ContourShading -> None];*)
-  (*  AppendTo[plotArgs, Contours -> Range[0, 60,0.25]];
-    AppendTo[ plotArgs,ContourStyle ->  Black];*)
-  (*  AppendTo[plotArgs, Exclusions->None];*)
+  AppendTo[plotArgs, Contours -> Range[0, 10 , 0.25]];
+  AppendTo[ plotArgs, ContourStyle -> Black];
+  (*AppendTo[plotArgs, Exclusions->None];*)
   (*  AppendTo[plotArgs, MaxRecursion -> 6];*)
   
   (* Print[plotArgs]; *)
   
-  (*   Apply[ContourPlot, plotArgs]*)
-  Apply[Plot3D, plotArgs]
+  Apply[ContourPlot, plotArgs]
+  (*Apply[Plot3D, plotArgs]*)
 ];
 
-graphTuningDamage[meantone, "TILT minimax-S", {{1201.699, 697.564}, "row"}]
+optimizeGeneratorTuningMap[meantone, "TILT minimax-U"]
+
+debug = True;
+optimizeGeneratorTuningMap[meantone, {"tuningSchemeSystematicName" -> "{2/1, 3/1, 3/2, 4/3, 5/2, 5/3, 5/4, 6/5, 81/80} minimax-U", "logging" -> True}]
+
+graphTuningDamage[meantone, "{2/1, 3/1, 3/2, 4/3, 5/2, 5/3, 5/4, 6/5} minimax-U", {{1200.000, 696.578}, "row"}]
+
+graphTuningDamage[meantone, "{2/1, 3/1, 3/2, 4/3, 5/2, 5/3, 5/4, 6/5, 81/80} minimax-U", {{1200.000, 696.578}, "row"}]
 
 graphTuningDamage["[⟨5 8 0] ⟨0 0 12]}", "TILT minimax-U", {{240.000, 232.945}, "row"}]
 
@@ -190,6 +197,10 @@ K = Transpose[{
 G = T.W.K.Inverse[M.T.W.K] // N
 j.G // N
 
+j.T.W // N
+
+{g1, g2}.M.T.W // N
+
 rZero = tZero - j
 B = T.W.Kprime.Inverse[Mprime.T.W.Kprime]
 rZero.B
@@ -203,3 +214,112 @@ gZero + gprime.Mprime
 {-3.107, -0.048, -7.213, 8.721}.{0.28023, 0.138848, -0.138848, -0.0934101} * {0, 1.657}
 
 {-3.107, -0.048, -7.213, 8.721}.{-0.28023, -0.138848, 0.138848, 0.0934101}
+
+j = 1200 * Log2[{{2, 3, 5}}];
+T = Transpose[{
+  {1, 0, 0},
+  {0, 1, 0},
+  {-1, 1, 0},
+  {2, -1, 0},
+  {-1, 0, 1},
+  {0, -1, 1},
+  {-2, 0, 1},
+  {1, 1, -1}
+}];
+W = DiagonalMatrix[Log2[Map[Numerator[#] * Denominator[#]&, {2 / 1, 3 / 1, 3 / 2, 4 / 3, 5 / 2, 5 / 3, 5 / 4, 6 / 5}]]];
+M = {{5, 8, 0}, {0, 0, 1}};
+
+j.T.W // MatrixForm // N
+{g1, g2}.M.T.W // MatrixForm // N
+
+K = Transpose[{{1, 0, 1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0, 0, 1}}];
+j.T.W.K // MatrixForm // N
+{g1, g2}.M.T.W.K // MatrixForm // N
+
+({236.43, 2754.56}.{{5, 8, 0}, {0, 0, 1}} - 1200 * Log2[{2, 3, 5}])
+
+
+
+Abs[{{{2, 3, 4}, {4, -4, 3}}, "row"}]
+
+r = {400, g2}.{{3, 0, 7}, {0, 1, 0}} - 1200 * Log2[{2, 3, 5}];
+
+Plot[
+  {
+    Abs[r.{0, 1, 0}],
+    Abs[r.{5, 0, -2}],
+    Abs[r.{-2, 0, 1}],
+    Abs[r.{0 , -1, 1}]
+  },
+  (*{g2, 106.56655,108.22392},*)
+  {g2, 1850, 1950},
+  PlotRange -> {0, 50},
+  PlotLabels -> "Expressions",
+  ImageSize -> 1000
+]
+
+
+
+(* HERE ON OUT IS MY FINAL EXAMPLE *)
+M = {{2, 3, 5, 6}, {0, 1, -2, -2}, {0, 0, -1, -1}};
+r = {600, g2, g3}.M - 1200 * Log2[{2, 3, 5, 7}];
+
+Plot3D[
+  {
+    (* the flat ceiling *)
+    Abs[r.{0, 0, -1, 1}], (* 7/5 *) (* yellow *)
+    
+    (* the ones that will make the tied line directly underneath that ceiling *)
+    Abs[r.{0, 2, 0, -1}], (* 9/7 *) (* blue *)
+    Abs[r.{0, 4, -2, 0}], (*81/25*)  (* green *)
+    
+    (* these two come in at an angle to that line, creating an interesting meta-diagonal line underneath *)
+    Abs[r.{-2, 0, 1, 0}], (* 5/4 *) (* red *)
+    Abs[r.{-2, 0, 0, 1}] (* 7/4 *) (* purple *)
+  },
+  {g2, 90, 115},
+  {g3, -5, 50},
+  PlotRange -> {0, 50},
+  ClippingStyle -> None,
+  MaxRecursion -> 6,
+  ImageSize -> 1000
+]
+
+(* HERE'S WITH A BIGGER TARGET-INTERVAL SET WITHOUT COMPROMISING RESULT, SO THIS VIEW LOOKS MORE INTERESTING / REALISTIC; BASICALLY GOTTA DROP 7/3 AND 7/6 FROM THE 10-TILT, ADD THIS 81/25, AND REMOVE 2/1 AND THINGS OFF BY 2/1 *)
+debug = True;
+optimizeGeneratorTuningMap["[⟨2 3 5 6] ⟨0 1 -2 -2] ⟨0 0 -1 -1]}", {"tuningSchemeSystematicName" -> "held-octave {3/2, 5/3, 5/4, 7/4, 7/5, 9/4, 9/5, 9/7,  81/25} minimax-U"}]
+
+Unprotect[D];
+j = 1200 * Log2[{2, 3, 5, 7}];
+L = DiagonalMatrix[Log2[{2, 3, 5, 7}]];
+D = {{0, 2.9146, -11.6585}}; (* that's -4x the change for g3 as for g2, so techincally it could be {{0,1,-4}}, but then the scale on the diagram wouldn't be quite right, i.e. from 0 to 1 the b1 value *)
+b = {b1};
+gZero = {600, 99.0404, 27.2641};(*{600,1899.04,2774.66};*)
+tZero = gZero.M;
+negativeRZero = j - tZero;
+
+damage[i_] := Abs[(b.D.M - negativeRZero).i];
+
+Plot[
+  {
+    damage[{-2, 2, 0, 0}], (* 9/4 *)
+    
+    damage[{0, 2, 0, -1}], (* 9/7 *) (* blue (but hidden with the other green one, which is fine) *)
+    damage[{0, 4, -2, 0}], (* 81/25 *) (* green *) (* looks tied with 9/7, but only in this cross-section; that's the point *)
+    
+    
+    damage[{-2, 0, 1, 0}], (* 5/4, same damage as 5/2 and 8/5 *) (* red *)
+    damage[{-2, 0, 0, 1}], (* 7/4 *) (* purple *)
+    
+    damage[{-1, 1, 0, 0}], (* 3/2 same damage as 3/1, 4/3, 8/3*)
+    damage[{0, -1, 1, 0}], (* 5/3, same damage as 6/5 *)
+    
+    damage[{0, 0, -1, 1}], (* 7/5, same damage as 10/7 *) (* yellow *)
+    damage[{0, 2, -1, 0}] (* 9/5 *)
+    
+  },
+  {b1, -0.25, 1.25},
+  PlotRange -> {0, 20},
+  PlotLabels -> "Expressions",
+  ImageSize -> 1000
+]
--- tuning/newer/generatorEmbeddingComputationMaterials.m
+++ tuning/newer/generatorEmbeddingComputationMaterials.m
@@ -1,4 +1,114 @@
-(* Mathematica Source File *)
-(* Created by the Wolfram Language Plugin for IntelliJ, see http://wlplugin.halirutan.de/ *)
-(* :Author: DouglasBlumeyer *)
-(* :Date: 2023-01-20 *)
+{{4.907, 5.129, 10.644, 5.492, 0, 3.585, 0, 5.807, 0, -6.170 }, {-4.907, -5.129, -15.966, 5.492, 0, -7.170, 5.170, -11.615, 11.955, 24.680}, {-9.814, -5.129, -10.644, -10.984, 5.392, 0, 0, -5.807, -5.977, 0}}.Transpose[{{0, 1, 1, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, -1, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, -1, 0, 0, 0, 0}}] // MatrixForm
+
+Unprotect[C];
+M = {{1, 1, 1, 2}, {0, 2, 3, 2}, {0, 0, 2, 1}};
+T = Transpose[{{1, 1, -1, 0}, {0, 0, -1, 1}, {3, 0, -1, 0}, {0, 2, -1, 0}, {-1, -1, 0, 1}, {2, -1, 0, 0}, {-1, 1, 0, 0}, {3, 0, 0, -1}, {0, 2, 0, -1}, {-3, 2, 0, 0}}];
+C = DiagonalMatrix[Log2[{30, 35, 40, 45, 42, 12, 6, 56, 63, 72}]];
+K = Transpose[{{0, 1, 1, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, -1, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, -1, 0, 0, 0, 0}}];
+
+
+Log[35] / Log[2] // N
+Log2[35] // N
+Log2[45] - Log2[35] // N
+Log2[9 / 7] // N
+
+M.T.C // MatrixForm
+
+M.T.C.K // MatrixForm
+M.T.C.K // N // MatrixForm
+
+Log2[56000] // N
+Log2[9 / 7] // N
+Log2[35 / 12] // N
+35 * 40^3
+Log2[144 / 35] // N
+Log2[45^2 / 35] // N
+Log2[35]
+
+T.C // MatrixForm
+
+T.C.K // MatrixForm
+T.C.K // N // MatrixForm
+
+Simplify[Inverse[M.T.C.K]] // MatrixForm
+Simplify[Inverse[M.T.C.K]] * Denominator[First[First[Simplify[Inverse[M.T.C.K]] ]]] // MatrixForm
+
+Numerator[First[First[Simplify[Inverse[M.T.C.K]] ]]]
+% // N
+
+Log[35] * Log[45] // N
+
+
+
+Log[35] / Log[2] // N
+Log2[35] // N
+
+Numerator[First[First[Simplify[Inverse[M.T.C.K]] ]]]
+% // N
+Denominator[First[First[Simplify[Inverse[M.T.C.K]] ]]]
+% // N
+
+Log[35]Log[45] // N
+Log2[35]Log2[45]Log[2]Log[2] // N
+
+
+Log2[E] // N
+
+fsk = Log[2];
+fsk * Log2[2](3 * fsk * Log2[35] * fsk * Log2[45] - 2 * fsk * Log2[12] * fsk * Log2[405 / 7]) // N
+fsk * fsk * fsk * (3Log2[35]Log2[45] - 2Log2[12]Log2[405 / 7]) // N
+
+fsk * fsk * fsk * (Log2[9 / 8]Log2[12]Log2[35] + Log2[40]Log2[45]Log2[20736 / 35]) // N
+
+(3Log2[35]Log2[45] - 2Log2[12]Log2[405 / 7]) / (Log2[9 / 8]Log2[12]Log2[35] + Log2[40]Log2[45]Log2[20736 / 35]) // N
+
+
+
+Log[35]Log[45] // N
+Log[35 * 45] // N
+
+35 * 40 * 40
+
+thing = Simplify[Inverse[M.T.C.K]];
+denom = Denominator[First[First[thing ]]];
+wereallywant = Simplify[Inverse[M.T.C.K]] * Denominator[First[First[Simplify[Inverse[M.T.C.K]] ]]];
+result = Simplify[T.C.K.wereallywant];
+
+Dimensions[result]
+result // MatrixForm
+result / denom // N // MatrixForm
+
+Part[Part[result], 4, 3]
+
+2^10.1559 * 3^1.83119 * 5^3.66237 * 7^-7.32475
+
+2^Part[Part[result / denom], 1, 1] * 3^Part[Part[result / denom], 2, 1] * 5^Part[Part[result / denom], 3, 1] * 7^Part[Part[result / denom], 4, 1] // N
+
+Log2[%] * 1200 // N
+    
+    
+    
+    Unprotect[C];
+M = {{1, 1, 1, 2}, {0, 2, 3, 2}, {0, 0, 2, 1}};
+T = Transpose[{{1, 1, -1, 0}, {0, 0, -1, 1}, {3, 0, -1, 0}, {0, 2, -1, 0}, {-1, -1, 0, 1}, {2, -1, 0, 0}, {-1, 1, 0, 0}, {3, 0, 0, -1}, {0, 2, 0, -1}, {-3, 2, 0, 0}, {0, -1, 1, 0}}];
+C = DiagonalMatrix[Log2[{30, 35, 40, 45, 42, 12, 6, 56, 63, 72, 2}]];
+K = Transpose[{{0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, -1, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}}];
+
+M.T.C // MatrixForm
+    
+    M.T.C.K // MatrixForm
+    
+    T.C.K // MatrixForm
+    T.C.K // N // MatrixForm
+    
+    T.C.K.Inverse[M.T.C.K] * Det[M.T.C.K] // N // MatrixForm
+    
+    T.C.K.Inverse[M.T.C.K] // N // MatrixForm
+    
+    T.C.K // N // MatrixForm
+    
+    Inverse[M.T.C.K] * Det[M.T.C.K] // N // MatrixForm
+    Det[M.T.C.K] // N
+    
+    j = 1200 * Log2[{2, 3, 5, 7}];
+j.T.C.K.Inverse[M.T.C.K] // N // MatrixForm
